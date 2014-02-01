2153e0984fc55a049018a7a125a54e28cec2b4d7
nathantross <nathantross@gmail.com>
2014-01-31 17:46:12 -0800

    created rails template application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DateApplication::Application.config.secret_key_base = '57e85f416866c5b6eaf5172c29586389af0a11e09ab5d7efbabbc5e3bfb15dafa0246e0ae37e2f3e75a01af24436a78374b6021de33849797ba0c44b100905fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DateApplication::Application.config.session_store :cookie_store, key: '_date_application_session'
