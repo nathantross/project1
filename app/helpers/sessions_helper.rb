6552c9127d634137daba4d9a5f8f965a4ee8fdd3
nathantross <nathantross@gmail.com>
2014-02-04 15:48:09 -0800

    Added microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,8 +23,16 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
+  
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
 
-    def sign_out
+
+  def sign_out
     current_user.update_attribute(:remember_token,
                 User.encrypt(User.new_remember_token))
     cookies.delete(:remember_token)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
@@ -9,12 +11,17 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
   
-def User.new_remember_token
+  def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
   end
 
   private
