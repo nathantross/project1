4d86e4687f3489b4500f6e36428e1f6f93aab266
nathantross <nathantross@gmail.com>
2014-02-06 05:26:31 -0800

    Updated Navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 DateApplication::Application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
